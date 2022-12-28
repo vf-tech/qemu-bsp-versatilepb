93e49110f68cbf47173a2172c2d521080ac8be1f
Mehmet Alinbay <mehmet.alinbay@pavotek.com.tr>
2022-12-28 12:45:24 +0300

    qemu_start --no-graphic kaldirildi. qemu artik kendi penceresinde aciliyor.

--- qemu_start.sh
+++ qemu_start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 ./qemu_bridge_setup.sh restart
-qemu-system-arm -M versatilepb -kernel buildroot-2018.08/output/images/zImage -dtb buildroot-2018.08/output/images/versatile-pb.dtb -drive file=buildroot-2018.08/output/images/rootfs.ext2,if=scsi -append "root=/dev/sda console=ttyAMA0,115200" -nographic -netdev tap,id=n1,ifname=tap0,script=no,downscript=no -device e1000,netdev=n1
+qemu-system-arm -M versatilepb -kernel buildroot-2018.08/output/images/zImage -dtb buildroot-2018.08/output/images/versatile-pb.dtb -drive file=buildroot-2018.08/output/images/rootfs.ext2,if=scsi -append "root=/dev/sda console=ttyAMA0,115200" -netdev tap,id=n1,ifname=tap0,script=no,downscript=no -device e1000,netdev=n1
 
 exit $?
