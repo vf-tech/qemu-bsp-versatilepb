aa3678cca7dd0164a11657eb5287df793524a26b
Mehmet Alinbay <mehmet.alinbay@pavotek.com.tr>
2023-01-06 09:18:03 +0300

    buildroot 2022.08'e gecildi. Kernel 4.16.7

--- qemu_start.sh
+++ qemu_start.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
 ./qemu_bridge_setup.sh restart
-qemu-system-arm -M versatilepb -kernel buildroot-2018.08/output/images/zImage -dtb buildroot-2018.08/output/images/versatile-pb.dtb -drive file=buildroot-2018.08/output/images/rootfs.ext2,if=scsi -append "root=/dev/sda console=ttyAMA0,115200" -netdev tap,id=n1,ifname=tap0,script=no,downscript=no -device e1000,netdev=n1
+#qemu-system-arm -M versatilepb -kernel buildroot--netdev tap,id=n1,ifname=tap0,script=no,downscript=no -device e1000,netdev=n1
+qemu-system-arm -M type=versatilepb \
+    -m 256M \
+    -kernel buildroot/output/images/zImage \
+    -dtb buildroot/output/images/versatile-pb.dtb \
+    -drive file=buildroot/output/images/rootfs.ext2,if=scsi,format=raw \
+    -append "console=ttyAMA0,115200 root=/dev/sda rw rootwait rootfstype=ext4" \
+    -netdev tap,id=n1,ifname=tap0,script=no,downscript=no -device rtl8139,netdev=n1
+    #-serial stdio 
 
 exit $?
